bbb52b113fedc8d8265468812e72cc91723a296a
pabgermone <pabgermone@gmail.com>
2017-03-05 00:21:12 -0300

    Initial commit

---
+++ domain/src/main/java/paradigmas/tpFinal/model/cliente/Cliente.java
@@ -0,0 +1,14 @@
+package paradigmas.tpFinal.model.cliente;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author Pablo Germone
+ */
+
+@Entity //Indica que esta clase sirve como entidad Hibernate
+@Table(name="Cliente") //Indica el nombre de la tabla en donde trabaja esta entidad
+public class Cliente {
+        
+}
