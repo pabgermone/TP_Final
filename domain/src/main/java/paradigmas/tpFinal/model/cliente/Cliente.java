604e07640cf6d91b098683ad81ce0300683b6b4d
pabgermone <pabgermone@gmail.com>
2017-03-05 02:12:19 -0300

    Clase Cliente Creada

--- domain/src/main/java/paradigmas/tpFinal/model/cliente/Cliente.java
+++ domain/src/main/java/paradigmas/tpFinal/model/cliente/Cliente.java
@@ -1,7 +1,6 @@
 package paradigmas.tpFinal.model.cliente;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * @author Pablo Germone
@@ -10,5 +9,79 @@ import javax.persistence.Table;
 @Entity //Indica que esta clase sirve como entidad Hibernate
 @Table(name="Cliente") //Indica el nombre de la tabla en donde trabaja esta entidad
 public class Cliente {
-        
+
+    /**
+     * Representa el ID de un cliente
+     * @Id: Anotacion que indica que se trata de una Primary Key
+     * @Column: Anotacion que indica el nombre de la columna de la tabla donde esta la Primary Key
+     * @GeneratedValue: Generador numerico para la columna indicada.
+     *                  El tipo de generacion es indicado por la variable strategy que puede tomar
+     *                  cualquier valor del tipo GenerationType.
+     *                  Pueden ser:
+     *                      AUTO: Indica que el tipo de proveedor BD debe elegir el tipo de estrategia mas
+     *                      conveniente.
+     *
+     *                      IDENTITY: Indica que el proveedor debe aignar un valor de Primary Key usando
+     *                      la columna de identidad.
+     *
+     *                      SEQUENCE: Indica que el proveedor debe asignar un valor usando una secuencia
+     *                      de la BD.
+     *
+     *                      TABLE: ??
+     */
+    @Id
+    @Column(name="ClienteID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="ClienteDNI")
+    private int dni;
+
+    @Column(name="ClienteNombre")
+    private String nombre;
+
+    @Column(name="ClienteApellido")
+    private String apellido;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+
+    @Override
+    public String toString(){
+        return "NroCliente: " + getId() + ", Nombre: " + getNombre() + ", Apellido: " + getApellido() + ", DNI: " + getDni();
+    }
 }
